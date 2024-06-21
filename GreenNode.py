8f8e92dd86f025a8c86931f550f2d603df23d53c
Zehra <109968769+zehragrbovic@users.noreply.github.com>
2024-06-21 22:20:30 +0100

    Gas and soil moisture analogue output adjusted

--- GreenNode.py
+++ GreenNode.py
@@ -2,6 +2,11 @@ import time
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import smbus
+from mq import *
+import spidev 
+from numpy import interp  
+import json
+import requests
 
 
 POWER_PIN = 12  	# GPIO pin that provides power to the rain sensor
@@ -55,6 +60,16 @@ def readLight(addr=DEVICE):
   data = bus.read_i2c_block_data(addr,ONE_TIME_HIGH_RES_MODE_1)
   return convertToNumber(data)
 
+# Read MCP3008 data
+def analogInput(channel):
+  spi = spidev.SpiDev() # Created an object
+  spi.open(0,0)
+	
+  spi.max_speed_hz = 1350000
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+
 def loop():
 	
 	# rain
@@ -66,8 +81,10 @@ def loop():
     GPIO.output(POWER_PIN, GPIO.LOW)  # turn the rain sensor's power OFF
 
     if rain_state == GPIO.HIGH:
+		#rd = false
         print("Rain state: No Rain Detected")
     else:
+		#rd = true
         print("Rain state: Rain Detected")
         
     # temperature and humidity
@@ -84,19 +101,59 @@ def loop():
 
     print(f"Gas State: {gas_state}")
     
+    mq = MQ();
+    perc = mq.MQPercentage()
+    lpg = perc["GAS_LPG"]
+    co = perc["CO"]
+    sm = perc["SMOKE"]
+    print(f"LPG: {lpg} ppm, CO: {co} ppm, Smoke: {sm} ppm")
+    
     # soil moisture
     if GPIO.input(DO_PIN_SOIL):
             print ("Soil state: Dry")
     else:
             print ("Soil state: Wet")
             
+
+    output = analogInput(0) # Reading from CH0
+    output = interp(output, [0, 1023], [100, 0])
+    output = int(output)
+    print("Moisture:", output)
+            
     # light intensity
-    lightLevel=readLight()
-    print("Light Level : " + format(lightLevel,'.2f') + " lx")
+    #lightLevel=readLight()
+    #print("Light Level : " + format(lightLevel,'.2f') + " lx")
     
     
     print()
-    time.sleep(3)  # pause for 1 second to avoid reading sensors frequently and prolong the sensor lifetime
+    
+    sensor_data = {
+		"temperature": temp,
+		"humidity": hum,
+		"carbon_monoxide": co,
+		"methane": sm,
+		"lpg": lpg,
+		"rain_detected": 0,
+		"soil_moisture": 0,
+		"illuminance": 200.0
+    }
+    
+    json_data = json.dumps(sensor_data)
+    
+    url = "https://9917-77-239-19-198.ngrok-free.app/api/measurements"
+    headers = {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "bilosta"
+    }
+    
+    #response = requests.post(url, data = json_data, headers = headers)
+    
+    #if response.status_code == 200:
+    #    print("Data sent successfully")
+    #else:
+    #    print("Response", response.text)
+    
+    time.sleep(10)  # pause for 10 seconds to avoid reading sensors frequently and prolong the sensor lifetime
 
 def cleanup():
     GPIO.cleanup()
